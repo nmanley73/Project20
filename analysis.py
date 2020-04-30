e7507b6b3e115e0580fc3d35f43df07401143e3a
Noel Manley <noelmanley@hotmail.com>
2020-04-30 10:09:21 +0100

    Bug fix and reupload histograms and scatter plots

--- analysis.py
+++ analysis.py
@@ -19,6 +19,7 @@ plt.title("Sepal length")
 plt.xlabel("Sepal length")
 plt.ylabel("number")
 plt.savefig("Fig 1. Sepal length.png")
+plt.clf()
 
 # Create histogram for Sepal Width. Add title, x label, y label and save 
 plt.hist(df["sepal_width"])
@@ -26,6 +27,7 @@ plt.xlabel("Sepal width")
 plt.ylabel("number")
 plt.title("Sepal width")
 plt.savefig("Fig 2. Sepal width.png")
+plt.clf()
 
 # Create histogram for Petal Length. Add title, x label, y label and save 
 plt.hist(df["petal_length"])
@@ -33,6 +35,7 @@ plt.title("petal length")
 plt.ylabel("number")
 plt.xlabel("Petal length")
 plt.savefig("Fig 3. Petal length.png")
+plt.clf()
 
 # Create histogram for Petal width. Add title, x label, y label and save 
 plt.hist(df["petal_width"])
@@ -40,6 +43,7 @@ plt.ylabel("number")
 plt.xlabel("Petal width")
 plt.title("Petal width")
 plt.savefig("Fig 4. Petal width.png")
+plt.clf()
 
 # Create scatter plot of sepal length against sepal width. Add title, xlabel, ylabel and save
 plt.plot(df["sepal_length"], df["sepal_width"],'b.')
@@ -47,27 +51,7 @@ plt.title("sepal length vs sepal width (cm)")
 plt.ylabel("sepal width (cm)")
 plt.xlabel("sepal length (cm)")
 plt.savefig("Fig 5. Sepal length vs sepal width.png")
-
-# Create scatter plot of petal length against petal width. Add title, xlabel, ylabel and save
-plt.plot(df["petal_length"], df["petal_width"],'b.')
-plt.xlabel("petal length (cm)")
-plt.ylabel("petal width (cm)")
-plt.title("petal length vs petal width (cm)")
-plt.savefig("Fig 6. Petal length vs petal width.png")
-
-# Create scatter plot of sepal length against petal length. Add title, xlabel, ylabel and save
-plt.plot(df["sepal_length"], df["petal_length"],'b.')
-plt.title("Sepal length vs petal length (cm)")
-plt.xlabel("sepal length (cm)")
-plt.ylabel("petal length (cm)")
-plt.savefig("Fig 7. Sepal length vs petal length.png")
-
-# Create scatter plot of sepal width against petal width. Add title, xlabel, ylabel and save
-plt.plot(df["sepal_width"], df["petal_width"],'b.')
-plt.title("Sepal width vs petal width (cm)")
-plt.ylabel("petal width (cm)")
-plt.xlabel("sepal width (cm)")
-plt.savefig("Fig 8. Sepal width vs petal width.png")
+plt.clf()
 
 # Create scatter plot of sepal length against sepal width by the 3 different species. Add title, legend, xlabel, ylabel and save
 plt.plot(iris_setosa["sepal_length"],iris_setosa["sepal_width"],'r.',label = 'Iris Setosa')
@@ -78,6 +62,15 @@ plt.xlabel("Sepal length (cm)")
 plt.ylabel("Sepal width (cm)")
 plt.title("Sepal length vs sepal width comparison by species type")
 plt.savefig("Fig 5.5 Sepal length vs sepal width by species.png")
+plt.clf()
+
+# Create scatter plot of petal length against petal width. Add title, xlabel, ylabel and save
+plt.plot(df["petal_length"], df["petal_width"],'b.')
+plt.xlabel("petal length (cm)")
+plt.ylabel("petal width (cm)")
+plt.title("petal length vs petal width (cm)")
+plt.savefig("Fig 6. Petal length vs petal width.png")
+plt.clf()
 
 # Create scatter plot of petal length against petal width by the 3 different species. Add title, legend, xlabel, ylabel and save
 plt.plot(iris_setosa["petal_length"], iris_setosa["petal_width"],'r.', label = 'Iris setosa')
@@ -87,7 +80,16 @@ plt.legend()
 plt.xlabel("petal length (cm)")
 plt.ylabel("petal width (cm)")
 plt.title("Petal length vs Petal width comparison by species type")
-plt.savefig("Fig 6.5 Petal length vs petal width by species")
+plt.savefig("Fig 6.5 Petal length vs petal width by species.png")
+plt.clf()
+
+# Create scatter plot of sepal length against petal length. Add title, xlabel, ylabel and save
+plt.plot(df["sepal_length"], df["petal_length"],'b.')
+plt.title("Sepal length vs petal length (cm)")
+plt.xlabel("sepal length (cm)")
+plt.ylabel("petal length (cm)")
+plt.savefig("Fig 7. Sepal length vs petal length.png")
+plt.clf()
 
 # Create scatter plot of sepal length against petal length by the 3 different species. Add title, legend, xlabel, ylabel and save
 plt.plot(iris_setosa["sepal_length"], iris_setosa["petal_length"],'r.', label = 'Iris setosa')
@@ -98,6 +100,15 @@ plt.xlabel("sepal length (cm)")
 plt.ylabel("petal length (cm)")
 plt.legend()
 plt.savefig("Fig 7.5 Sepal length vs petal length by species.png")
+plt.clf()
+
+# Create scatter plot of sepal width against petal width. Add title, xlabel, ylabel and save
+plt.plot(df["sepal_width"], df["petal_width"],'b.')
+plt.title("Sepal width vs petal width (cm)")
+plt.ylabel("petal width (cm)")
+plt.xlabel("sepal width (cm)")
+plt.savefig("Fig 8. Sepal width vs petal width.png")
+plt.clf()
 
 # Create scatter plot of sepal width against petal width by the 3 different species. Add title, legend, xlabel, ylabel and save
 plt.plot(iris_setosa["sepal_width"], iris_setosa["petal_width"],'r.', label = 'Iris setosa')
@@ -108,6 +119,8 @@ plt.title("Sepal width vs Petal width comparison by species type")
 plt.ylabel("petal width (cm)")
 plt.xlabel("sepal width (cm)")
 plt.savefig("Fig 8.5 Sepal width vs petal width by species.png")
+plt.clf()
+
 
 
 
